0e69c68454ede798232f7fe175a9397176dd0c40
Burenkovuser <nb2207@yandex.ru>
2019-10-14 17:15:03 +0700

    UI is ready

---
+++ ToDoFirebase/Controllers/TasksViewController.swift
@@ -0,0 +1,48 @@
+//
+//  TasksViewController.swift
+//  ToDoFirebase
+//
+//  Created by Vasilii on 14.10.2019.
+//  Copyright © 2019 Vasilii Burenkov. All rights reserved.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "This is cell mumber \(indexPath.row)"
+        cell.textLabel?.textColor = .white
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+    }
+    
+}
